663ea1f0f04bdeb60eb726d056f98ae5a5ca78c0
Eli-Vortex <1712125046@qq.com>
2025-11-20 11:04:11 +0800

    Add all files with LFS pointers for large binaries

---
+++ projectV3/app/build/intermediates/cxx/Release/466a2e6h/logs/x86_64/build_command_targets.bat
@@ -0,0 +1,9 @@
+@echo off
+"C:\\Users\\Administrator\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "E:\\QQ\\projectV3\\projectV3\\projectV3\\app\\.cxx\\Release\\466a2e6h\\x86_64" ^
+  ggml ^
+  ggml-base ^
+  ggml-cpu ^
+  llama ^
+  llama-android
